3590f7694cf73afa85b2be215ed65976fe9efef5
wondwizard <148422096+wondwizard@users.noreply.github.com>
2024-03-13 08:39:21 -0500

    created the function for playing the game

--- javascript.js
+++ javascript.js
@@ -4,4 +4,19 @@ function getComputerChoice() {
     return choice[Math.floor(Math.random()*choice.length)];
 }
 
-console.log('The computer chose ' + getComputerChoice());
+let computerChoice = console.log('The computer chose ' + getComputerChoice());
+
+function round (playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        console.log('It is a tie! You and the computer both chose ' + computerChoice);
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        console.log('You won! The computer chose ' + computerChoice);
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        console.log('You won! The computer chose ' + computerChoice);
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        console.log('You won! The computer chose ' + computerChoice);
+    } else {
+        console.log('You loose! The computer chose ' + computerChoice);
+    }
+
+}
