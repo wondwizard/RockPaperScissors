a2b033a303cb50d8cf08709b6f3196fd495c262a
wondwizard <148422096+wondwizard@users.noreply.github.com>
2024-03-13 09:49:31 -0500

    created a play game function that will play the game five times and then count up the score and give you the results

--- javascript.js
+++ javascript.js
@@ -1,28 +1,54 @@
 const choice = ['rock', 'paper', 'scissors'];
 
+
 function getComputerChoice() {
     return choice[Math.floor(Math.random()*choice.length)];
 }
 
-const computerChoice = getComputerChoice();
-console.log('The computer chose ' + computerChoice);
 
 function round (playerSelection, computerSelection) {
-    console.log('You chose ' + playerSelection);
     if (playerSelection == computerSelection) {
-        return ('It is a tie! You and the computer both chose ' + computerChoice);
+        return ('It is a tie!');
     } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        return ('You won! The computer chose ' + computerChoice);
+        return ('You won!');
     } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        return ('You won! The computer chose ' + computerChoice);
+        return ('You won!');
     } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        return ('You won! The computer chose ' + computerChoice);
+        return ('You won!');
     } else {
-        return ('You loose! The computer chose ' + computerChoice);
+        return ('You loose!');
     }
 
 }
 
-const playerChoice = prompt('Rock, Paper, or Scissors?').toLowerCase();
 
-console.log(round(playerChoice, computerChoice));
+function playGame () {
+    let i = 5
+    computerScore = 0
+    playerScore = 0
+    while (i > 0) {
+        let computerChoice = getComputerChoice();
+        let playerChoice = prompt('Rock, Paper, or Scissors?').toLowerCase();
+        console.log('The computer chose ' + computerChoice);
+        console.log('You chose ' + playerChoice);
+        console.log(round(playerChoice, computerChoice));
+        if (round(playerChoice, computerChoice) === ('You loose!')) {
+            computerScore++
+        } else if (round(playerChoice, computerChoice) === ('You won!')) {
+            playerScore++
+        }
+        i--;        
+    }
+    console.log('You had ' + playerScore + ' points');
+    console.log('The computer had ' + computerScore + ' points');
+    if (computerScore > playerScore) {
+        console.log('Sorry the computer won :(');
+    } else if (computerScore < playerScore) {
+        console.log('You won!');
+    } else {
+        console.log('It is a tie.');
+    }
+}
+
+
+playGame()
